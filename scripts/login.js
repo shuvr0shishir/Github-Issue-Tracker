51adce44cd1f542e269c502fde61576cadfab712
shuvr0shishir <shuvroshishirx@gmail.com>
2026-03-08 01:25:03 +0600

    Login Functionality added

---
+++ scripts/login.js
@@ -0,0 +1,23 @@
+// Login Functionalities
+
+const loginBtn = getById('login-btn')
+loginBtn.addEventListener('click', () => {
+    const warnings = document.querySelectorAll('.warning')
+    warnings.forEach(w => {
+        w.classList.add('hidden')
+    });
+
+    const username = getById('username');
+    const password = getById('password');
+
+    if (username.value.trim() === 'admin' && password.value.trim() === 'admin123') {
+        window.location.assign('./main.html')
+    } else {
+        alert('username or password wrong!');
+        username.value = ''
+        password.value = ''
+        warnings.forEach(w => {
+            w.classList.remove('hidden')
+        });
+    }
+});
